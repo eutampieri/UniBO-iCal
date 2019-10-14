a4ca145a5e3ae01b571e4756fef74bb5f1770d39
Eugenio Tampieri <eugenio@eutampieri.eu>
2019-10-14 09:05:15 +0000

    Fixed cal generation script

--- cal.php
+++ cal.php
@@ -1,18 +1,23 @@
 <?php
 header("Content-Type: text/calendar");
-$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."orario-lezioni/@@orario_reale_json?anno=".$_GET["anno"];
-$dati = json_decode(file_get_contents($url));
+require('ical.php');
+date_default_timezone_set('Europe/Rome');
+$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/orario-lezioni/@@orario_reale_json?anno=".$_GET["anno"];
+error_log($url);
+$dati = json_decode(file_get_contents($url), true);
 $orario = [];
-foreach($dati["events"] as $evento) {
-	$data = strptime($lezione["start"], "%FT%T");
-	$fine = strptime($lezione["end"], "%FT%T");
+foreach($dati["events"] as $lezione) {
+	$data = date_create_from_format("Y-m-d\TH:i:s", $lezione["start"]);
+	$fine = date_create_from_format("Y-m-d\TH:i:s", $lezione["end"]);
 	$descrizione = $lezione["title"].' - '.$lezione["aule"][0]["des_risorsa"];
 	array_push($orario, [
-		"start" =>$data,
-		"end" => $data+3600,
+		"start" => date_format($data, "U"),
+		"end" => date_format($fine, "U"),
 		"desc" => $descrizione
 	]);
 }
-$cal = new iCalendar();
+$cal = new iCalendar(intval($_GET["reminder"]);
 echo $cal->ical($orario);
 ?>
+
+
