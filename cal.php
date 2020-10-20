a3981e09fe7eeec23507f016acbe4a4dae89d00c
Eugenio Tampieri <eugenio@eutampieri.eu>
2020-10-20 13:24:11 +0200

    Use FrancescoBonzi's service

--- cal.php
+++ cal.php
@@ -1,27 +1,14 @@
 <?php
 header("Content-Type: text/calendar");
 require('PHPiCal/ical.php');
-date_default_timezone_set('Europe/Rome');
-$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/orario-lezioni/@@orario_reale_json?anno=".$_GET["anno"];
-$raw = file_get_contents($url);
-if($raw === false) {
-	$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/timetable/@@orario_reale_json?anno=".$_GET["anno"];
+$url = null;
+if(file_get_contents("https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/orario-lezioni/@@orario_reale_json?anno=".$_GET["anno"]) === false) {
+	$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/timetable";
 	$raw = file_get_contents($url);
+} else {
+	$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/orario-lezioni";
 }
-$dati = json_decode($raw, true);
-$orario = [];
-foreach($dati["events"] as $lezione) {
-	$data = date_create_from_format("Y-m-d\TH:i:s", $lezione["start"]);
-	$fine = date_create_from_format("Y-m-d\TH:i:s", $lezione["end"]);
-	$descrizione = $lezione["title"].' - '.$lezione["aule"][0]["des_risorsa"];
-	array_push($orario, [
-		"start" => date_format($data, "U"),
-		"end" => date_format($fine, "U"),
-		"desc" => $descrizione
-	]);
-}
-$cal = new iCalendar(intval($_GET["reminder"]));
-echo $cal->ical($orario);
+header("Location: http://unibocalendar.duckdns.org/get_ical?timetable_url=$url&year=".$_GET["anno"])
 ?>
 
 
