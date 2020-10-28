ba451236a41ac77c6ba9897db4e1cd894c9421ea
Eugenio Tampieri <eugenio@eutampieri.eu>
2020-10-28 10:50:24 +0100

    Compat fix for FrancescoBonzi's new URL

--- cal.php
+++ cal.php
@@ -1,13 +1,7 @@
 <?php
 header("Content-Type: text/calendar");
 require('PHPiCal/ical.php');
-$url = null;
-if(file_get_contents("https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/orario-lezioni/@@orario_reale_json?anno=".$_GET["anno"]) === false) {
-	$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/timetable";
-	$raw = file_get_contents($url);
-} else {
-	$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"]."/orario-lezioni";
-}
+$url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"];
 echo file_get_contents("http://unibocalendar.duckdns.org/get_ical?timetable_url=$url&year=".$_GET["anno"]."&alert=".strval(intval($_GET["reminder"])*60))
 ?>
 
