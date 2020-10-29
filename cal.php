981ee7f69335cf609e074fafe883d8cbadb4e68d
Eugenio Tampieri <eugenio@eutampieri.eu>
2020-10-29 16:40:37 +0100

    Passing UUID to FrancescoBonzi

--- cal.php
+++ cal.php
@@ -2,7 +2,7 @@
 header("Content-Type: text/calendar");
 require('PHPiCal/ical.php');
 $url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"];
-echo file_get_contents("http://unibocalendar.duckdns.org/get_ical?timetable_url=$url&year=".$_GET["anno"]."&alert=".strval(intval($_GET["reminder"])*60))
+echo file_get_contents("http://unibocalendar.duckdns.org/get_ical?timetable_url=$url&year=".$_GET["anno"]."&alert=".strval(intval($_GET["reminder"])*60)."&uuid=eu.eutampieri.cal-unibo.".md5($_SERVER["HTTP_CF_CONNECTING_IP"]));
 ?>
 
 
