d4d6cbcc207c29730f1b71146dab94945704058c
Eugenio Tampieri <eugenio@eutampieri.eu>
2020-10-29 17:58:05 +0100

    Sendo only cookie UUID

--- cal.php
+++ cal.php
@@ -9,7 +9,7 @@ if(!isset($_COOKIE["uuid"])) {
 header("Content-Type: text/calendar");
 require('PHPiCal/ical.php');
 $url = "https://corsi.unibo.it/".$_GET["tipoCorso"].'/'.$_GET["nomeCorso"];
-echo file_get_contents("http://unibocalendar.duckdns.org/get_ical?timetable_url=$url&year=".$_GET["anno"]."&alert=".strval(intval($_GET["reminder"])*60)."&uuid=eu.eutampieri.cal-unibo.".$id.'.'.md5($_SERVER["HTTP_CF_CONNECTING_IP"]));
+echo file_get_contents("http://unibocalendar.duckdns.org/get_ical?timetable_url=$url&year=".$_GET["anno"]."&alert=".strval(intval($_GET["reminder"])*60)."&uuid=".$id);
 ?>
 
 
